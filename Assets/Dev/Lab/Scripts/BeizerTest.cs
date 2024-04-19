865faa02c439203b243c1cfefb0207741bf3f7c0
TyoukabuN <40262830+TyoukabuN@users.noreply.github.com>
2024-04-20 02:18:49 +0800

    beizer

---
+++ Assets/Dev/Lab/Scripts/BeizerTest.cs
@@ -0,0 +1,39 @@
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeizerTest : MonoBehaviour
+{
+    public List<Transform> transforms = new List<Transform>();
+
+    private List<Vector3> wpoints
+    {
+        get
+        {
+            var temp = new List<Vector3>();
+            foreach (var trans in transforms)
+            {
+                temp.Add(trans.position);
+            }
+            return temp;
+        }
+    }
+
+    private void Update()
+    {
+        PrintCurve();
+    }
+
+    [Button("PrintCurve")]
+    public void PrintCurve()
+    {
+        var points = BeizerUtil.SampleBeizerCurve(wpoints, 20);
+        for (int i = 0; i < points.Count - 1; i++)
+        {
+            Vector3 p0 = points[i];
+            Vector3 p1 = points[i + 1];
+            Debug.DrawLine(p0, p1, Color.green, Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Dev/Scripts/Utility/BeizerUtil.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class BeizerUtil 
+{
+    private static Vector3[] vec3Cache = new Vector3[100];
+
+    static BeizerUtil()
+    {
+        if (vec3Cache == null || vec3Cache.Length <= 0)
+            vec3Cache = new Vector3[100];
+    }
+
+    public static List<Vector3> SampleBeizerCurve(List<Vector3> plist, int step)
+    {
+        if(plist.Count <= 1)
+            return null;
+        var temp = new List<Vector3>();
+        for (int i = 0; i < step + 1; i++)
+        {
+            float t = (float)i / step;
+            Vector3 point;
+            if (t <= 0)
+                point = plist[0];
+            else if (t >= 1)
+                point = plist[plist.Count - 1];
+            else
+                point = DegNBeizer(t, plist.ToArray(), plist.Count);
+            temp.Add(point);
+        }
+        return temp;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="t"></param>
+    /// <param name="plist"></param>
+    /// <param name="count">用来减少list的创建</param>
+    /// <returns></returns>
+    public static Vector3 DegNBeizer(float t, Vector3[] plist, int count)
+    {
+        if (count == 2)
+            return LinearBeizer(plist[0], plist[1], t);
+        int index = 0;
+        for (int i = 0; i < count - 1; i++, index++)
+        {
+            vec3Cache[index] = LinearBeizer(plist[i], plist[i + 1], t);
+        }
+        return DegNBeizer(t, vec3Cache, index);
+    }
+    public static Vector3 LinearBeizer(Vector3 p0, Vector3 p1, float t)
+    {
+        return (1 - t) * p0 + t * p1;
+    }
+}

80aba0b2d195f5fc7cb6c1afaddc697477e5a568
TyoukabuN <40262830+TyoukabuN@users.noreply.github.com>
2024-04-24 18:36:55 +0800

    animationUtil

--- Assets/Dev/Lab/Scripts/AnimationTest.cs
+++ Assets/Dev/Lab/Scripts/AnimationTest.cs
@@ -1,3 +1,4 @@
+using PJR;
 using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
@@ -22,59 +23,30 @@ public class AnimationTest : MonoBehaviour
         {
             elapsedTime = 0f;
         }
-        CurveTest();
+        CurveTest(elapsedTime);
     }
 
 
-    [Button(nameof(CurveTest))]
-    public void CurveTest()
+    public void CurveTest(float animationTime)
     {
-
-
         Vector3 localPosition = Vector3.zero;
         Quaternion localRotation = Quaternion.identity;
         Vector3 localScale = Vector3.zero;
         if (animationClip != null)
         {
-            var editorCurveBindings = AnimationUtility.GetCurveBindings(animationClip);
-            foreach (var curveBinding in editorCurveBindings)
+            if (AnimationUtil.TryGetTranssformCurveHandle(animationClip, "root", out var handle))
             {
-                if (curveBinding.type != typeof(Transform))
-                    continue;
-                if (curveBinding.path == "root")
-                { 
-                    Debug.Log($"{curveBinding.path}/{curveBinding.propertyName}  Type: {curveBinding.type}");
-                    var curve = AnimationUtility.GetEditorCurve(animationClip, curveBinding);
-
-                    if (curveBinding.propertyName == "m_LocalPosition.x")
-                        localPosition.x = curve.Evaluate(elapsedTime);
-                    if (curveBinding.propertyName == "m_LocalPosition.y")
-                        localPosition.y = curve.Evaluate(elapsedTime);
-                    if (curveBinding.propertyName == "m_LocalPosition.z")
-                        localPosition.z = curve.Evaluate(elapsedTime);
-
-                    if (curveBinding.propertyName == "m_LocalRotation.x")
-                        localRotation.x = curve.Evaluate(elapsedTime);
-                    if (curveBinding.propertyName == "m_LocalRotation.y")
-                        localRotation.y = curve.Evaluate(elapsedTime);
-                    if (curveBinding.propertyName == "m_LocalRotation.z")
-                        localRotation.z = curve.Evaluate(elapsedTime);
-                    if (curveBinding.propertyName == "m_LocalRotation.w")
-                        localRotation.w = curve.Evaluate(elapsedTime);
-
-                    if (curveBinding.propertyName == "m_LocalScale.x")
-                        localScale.x = curve.Evaluate(elapsedTime);
-                    if (curveBinding.propertyName == "m_LocalScale.y")
-                        localScale.y = curve.Evaluate(elapsedTime);
-                    if (curveBinding.propertyName == "m_LocalScale.z")
-                        localScale.z = curve.Evaluate(elapsedTime);
-                }
+                handle.Evaluate(target, animationTime);
             }
-            if (target != null)
-            {
-                target.localPosition = localPosition;
-                target.localRotation = localRotation;
-            }
+        }
+    }
+
+    [Button("测试")]
+    public void Test()
+    {
+        if (!EditorApplication.isPlaying)
+        {
+            EditorApplication.isPlaying = true;
         }
     }
 }
--- Assets/Dev/Scripts/Utility/AnimationUtil.cs
+++ Assets/Dev/Scripts/Utility/AnimationUtil.cs
@@ -3,95 +3,98 @@ using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
-public static class AnimationUtil
+namespace PJR
 {
-    private static Dictionary<AnimationClip, TransformCurveHandle> clip2transCurveHandle = new Dictionary<AnimationClip, TransformCurveHandle>();
-
-    public static bool TryGetTranssformCurveHandle(AnimationClip animationClip, string path, out TransformCurveHandle handle)
+    public static class AnimationUtil
     {
-        handle = null;
-        path = string.IsNullOrEmpty(path) ? "root" : path;
-        if (animationClip == null)
-            return false; ;
-        clip2transCurveHandle ??= new Dictionary<AnimationClip, TransformCurveHandle>();
-        if (!clip2transCurveHandle.TryGetValue(animationClip, out handle))
-        {
-            handle = new TransformCurveHandle(animationClip, path);
-            clip2transCurveHandle[animationClip] = handle;
-        }
+        private static Dictionary<AnimationClip, TransformCurveHandle> clip2transCurveHandle = new Dictionary<AnimationClip, TransformCurveHandle>();
 
-        return true;
-    }
-    /// <summary>
-    /// 直接获取AnimationClip中root节点Transform相关的curve来实现rootMotion
-    /// 其他地方应该可以用到，但是先放在这里
-    /// </summary>
-    public class TransformCurveHandle
-    {
-        public AnimationCurve localPositionX;
-        public AnimationCurve localPositionY;
-        public AnimationCurve localPositionZ;
-        public AnimationCurve localRotationX;
-        public AnimationCurve localRotationY;
-        public AnimationCurve localRotationZ;
-        public AnimationCurve localRotationW;
-        public AnimationCurve localScaaleX;
-        public AnimationCurve localScaaleY;
-        public AnimationCurve localScaaleZ;
-
-        public TransformCurveHandle(AnimationClip animationClip, string path)
+        public static bool TryGetTranssformCurveHandle(AnimationClip animationClip, string path, out TransformCurveHandle handle)
         {
-            if (animationClip == null || string.IsNullOrEmpty(path))
-                return;
-            var editorCurveBindings = AnimationUtility.GetCurveBindings(animationClip);
-            foreach (var curveBinding in editorCurveBindings)
+            handle = null;
+            path = string.IsNullOrEmpty(path) ? "root" : path;
+            if (animationClip == null)
+                return false; ;
+            clip2transCurveHandle ??= new Dictionary<AnimationClip, TransformCurveHandle>();
+            if (!clip2transCurveHandle.TryGetValue(animationClip, out handle))
             {
-                if (curveBinding.type != typeof(Transform))
-                    continue;
-                if (curveBinding.path != path)
-                    continue;
-
-                var curve = AnimationUtility.GetEditorCurve(animationClip, curveBinding);
-
-                if (curveBinding.propertyName == "m_LocalPosition.x") localPositionX = curve;
-                if (curveBinding.propertyName == "m_LocalPosition.y") localPositionY = curve;
-                if (curveBinding.propertyName == "m_LocalPosition.z") localPositionZ = curve;
-
-                if (curveBinding.propertyName == "m_LocalRotation.x") localRotationX = curve;
-                if (curveBinding.propertyName == "m_LocalRotation.y") localRotationY = curve;
-                if (curveBinding.propertyName == "m_LocalRotation.z") localRotationZ = curve;
-                if (curveBinding.propertyName == "m_LocalRotation.w") localRotationW = curve;
-
-                if (curveBinding.propertyName == "m_LocalScale.x") localScaaleX = curve;
-                if (curveBinding.propertyName == "m_LocalScale.y") localScaaleY = curve;
-                if (curveBinding.propertyName == "m_LocalScale.z") localScaaleZ = curve;
+                handle = new TransformCurveHandle(animationClip, path);
+                clip2transCurveHandle[animationClip] = handle;
             }
+
+            return true;
         }
-
-        public void Evaluate(Transform target, float time)
+        /// <summary>
+        /// 直接获取AnimationClip中root节点Transform相关的curve来实现rootMotion
+        /// 其他地方应该可以用到，但是先放在这里
+        /// </summary>
+        public class TransformCurveHandle
         {
-            if (target == null) return;
+            public AnimationCurve localPositionX;
+            public AnimationCurve localPositionY;
+            public AnimationCurve localPositionZ;
+            public AnimationCurve localRotationX;
+            public AnimationCurve localRotationY;
+            public AnimationCurve localRotationZ;
+            public AnimationCurve localRotationW;
+            public AnimationCurve localScaaleX;
+            public AnimationCurve localScaaleY;
+            public AnimationCurve localScaaleZ;
 
-            Vector3 localPosition = Vector3.zero;
-            Quaternion localRotation = Quaternion.identity;
-            Vector3 localScale = Vector3.zero;
+            public TransformCurveHandle(AnimationClip animationClip, string path)
+            {
+                if (animationClip == null || string.IsNullOrEmpty(path))
+                    return;
+                var editorCurveBindings = AnimationUtility.GetCurveBindings(animationClip);
+                foreach (var curveBinding in editorCurveBindings)
+                {
+                    if (curveBinding.type != typeof(Transform))
+                        continue;
+                    if (curveBinding.path != path)
+                        continue;
 
-            if (localPositionX != null) localPosition.x = localPositionX.Evaluate(time);
-            if (localPositionY != null) localPosition.y = localPositionY.Evaluate(time);
-            if (localPositionZ != null) localPosition.z = localPositionZ.Evaluate(time);
+                    var curve = AnimationUtility.GetEditorCurve(animationClip, curveBinding);
 
-            if (localRotationX != null) localRotation.x = localRotationX.Evaluate(time);
-            if (localRotationY != null) localRotation.y = localRotationY.Evaluate(time);
-            if (localRotationZ != null) localRotation.z = localRotationZ.Evaluate(time);
-            if (localRotationW != null) localRotation.w = localRotationW.Evaluate(time);
+                    if (curveBinding.propertyName == "m_LocalPosition.x") localPositionX = curve;
+                    if (curveBinding.propertyName == "m_LocalPosition.y") localPositionY = curve;
+                    if (curveBinding.propertyName == "m_LocalPosition.z") localPositionZ = curve;
 
-            if (localScaaleX != null) localScale.x = localScaaleX.Evaluate(time);
-            if (localScaaleY != null) localScale.y = localScaaleY.Evaluate(time);
-            if (localScaaleZ != null) localScale.z = localScaaleZ.Evaluate(time);
+                    if (curveBinding.propertyName == "m_LocalRotation.x") localRotationX = curve;
+                    if (curveBinding.propertyName == "m_LocalRotation.y") localRotationY = curve;
+                    if (curveBinding.propertyName == "m_LocalRotation.z") localRotationZ = curve;
+                    if (curveBinding.propertyName == "m_LocalRotation.w") localRotationW = curve;
 
-            target.localPosition = localPosition;
-            target.localRotation = localRotation;
-            target.localScale = localScale;
+                    if (curveBinding.propertyName == "m_LocalScale.x") localScaaleX = curve;
+                    if (curveBinding.propertyName == "m_LocalScale.y") localScaaleY = curve;
+                    if (curveBinding.propertyName == "m_LocalScale.z") localScaaleZ = curve;
+                }
+            }
+
+            public void Evaluate(Transform target, float time)
+            {
+                if (target == null) return;
+
+                Vector3 localPosition = Vector3.zero;
+                Quaternion localRotation = Quaternion.identity;
+                Vector3 localScale = Vector3.zero;
+
+                if (localPositionX != null) localPosition.x = localPositionX.Evaluate(time);
+                if (localPositionY != null) localPosition.y = localPositionY.Evaluate(time);
+                if (localPositionZ != null) localPosition.z = localPositionZ.Evaluate(time);
+
+                if (localRotationX != null) localRotation.x = localRotationX.Evaluate(time);
+                if (localRotationY != null) localRotation.y = localRotationY.Evaluate(time);
+                if (localRotationZ != null) localRotation.z = localRotationZ.Evaluate(time);
+                if (localRotationW != null) localRotation.w = localRotationW.Evaluate(time);
+
+                if (localScaaleX != null) localScale.x = localScaaleX.Evaluate(time);
+                if (localScaaleY != null) localScale.y = localScaaleY.Evaluate(time);
+                if (localScaaleZ != null) localScale.z = localScaaleZ.Evaluate(time);
+
+                target.localPosition = localPosition;
+                target.localRotation = localRotation;
+                target.localScale = localScale;
+            }
         }
     }
 }

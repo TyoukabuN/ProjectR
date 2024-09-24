b818ddcb4be716eb69859477cfba62e3f0986a62
TyoukabuN <40262830+TyoukabuN@users.noreply.github.com>
2024-09-24 16:31:20 +0800

    重命名工具

--- Assets/Dev/Scripts/Editor/AssetBundleBuild/AssetReferenceLeftToolbar.cs
+++ Assets/Dev/Scripts/Editor/AssetBundleBuild/AssetReferenceLeftToolbar.cs
@@ -2,10 +2,6 @@ using System;
 using UnityEditor;
 using UnityEngine;
 using InfinityCode.UltimateEditorEnhancer;
-using Sirenix.Utilities.Editor;
-using System.Linq;
-using NUnit.Framework;
-using UnityEditor.Build.Player;
 
 namespace PJR.Editor
 {
@@ -60,6 +56,8 @@ namespace PJR.Editor
             menu.AddItem(new GUIContent("Asset引用相关/刷新引用[并清理缓存]"), false, () => { AssetAnalysisSetting.ClearCacheAndRefreshReferenceData(); });
             menu.AddItem(new GUIContent("Asset引用相关/Ping当前外部引用检查目录"), false, () => { AssetAnalysisSetting.PingRootForOutsideReferedCheck(); });
             menu.AddItem(new GUIContent("Asset引用相关/设置需要显示引用数的路径"), false, () => { AssetAnalysisSetting.GetAsset(); AssetAnalysisSetting.OpenAsset(); });
+            //
+            menu.AddItem(new GUIContent("Asset相关工具/批量重命名窗口"), false, () => { BatchRename.BatchRenameWindow.Open(); });
             //
             menu.AddItem(new GUIContent("Debug/Setting/ResourceSystem.DebugLevel/0"), ResourceSystem.DebugLevel == 0, () => { ResourceSystem.DebugLevel = 0; });
             menu.AddItem(new GUIContent("Debug/Setting/ResourceSystem.DebugLevel/1"), ResourceSystem.DebugLevel == 1, () => { ResourceSystem.DebugLevel = 1; });

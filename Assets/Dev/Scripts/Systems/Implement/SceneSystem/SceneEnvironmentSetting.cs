4c8759c425051cdfcd4e71c81c0d66690399032d
TyoukabuN <40262830+TyoukabuN@users.noreply.github.com>
2024-11-05 11:37:55 +0800

    Editor

--- Assets/Dev/Scripts/Systems/Implement/SceneSystem/SceneEnvironmentSetting.cs
+++ Assets/Dev/Scripts/Systems/Implement/SceneSystem/SceneEnvironmentSetting.cs
@@ -1,10 +1,14 @@
+using Sirenix.OdinInspector;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SceneEnvironmentSetting : MonoBehaviour
 {
+    [LabelText("生成怪物")]
     public bool GenSceneMonster = true;
+    [LabelText("生成陷阱")]
     public bool GenSceneTrap = true;
+    [LabelText("生成物品")]
     public bool GenSceneItem = true;
 }

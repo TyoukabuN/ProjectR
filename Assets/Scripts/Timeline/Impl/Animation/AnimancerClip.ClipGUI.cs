94a532884292dddbe02ec55b88aa89691f7aab23
TyoukabuN <40262830+TyoukabuN@users.noreply.github.com>
2025-05-27 11:37:07 +0800

    pjr.Timeline

---
+++ Assets/Scripts/Timeline/Impl/Animation/AnimancerClip.ClipGUI.cs
@@ -0,0 +1,12 @@
+using PJR.Timeline.Editor;
+#if UNITY_EDITOR
+#endif
+
+namespace PJR.Timeline
+{
+    [BindingClipGUI(typeof(AnimancerClip))]
+    public class AnimancerClip_ClipGUI : ClipGUI
+    {
+        
+    }
+}

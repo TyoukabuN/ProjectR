b4291b76024cdee488366fa12affcf2363451ce8
TyoukabuN <745642803@qq.com>
2026-04-02 00:12:14 +0800

    feat(timeline): 添加IClip接口定义
    - 定义IClip接口及其属性和方法
    - 包含帧率类型、静音、描述等基本属性
    - 添加时间段起止、长度及帧相关计算属性
    - 提供获取剪辑名称、信息及运行器的方法
    - 在编辑器模式下添加预览运行器和上下文菜单方法声明
    - 支持Unity编辑器相关的条件编译逻辑

---
+++ Assets/Scripts/Timeline/Runtime/Unit/IClip.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace PJR.Timeline
+{
+    public interface IClip
+    {
+        Define.EFrameRate FrameRateType { get; set; }
+        bool Mute { get; set; }
+        string Description { get; set; }
+        double start { get; set; }
+        double end { get; set; }
+        double duration { get; }
+        int TotalFrame { get; }
+        int StartFrame { get; set;}
+        int EndFrame { get; set;}
+        string GetClipName();
+        string GetClipInfo();
+        public ClipRunner GetRunner();
+        public Color GetClipColor();
+#if UNITY_EDITOR
+        public ClipRunner Editor_GetPreviewRunner();
+        public void GetContextMenu(GenericMenu menu){}
+#endif
+    }
+}
